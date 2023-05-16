38157a11fee0d028f928f122ce96fbb75d9dc038
SolSol5974 <105468454+SolSol5974@users.noreply.github.com>
2023-05-16 12:51:48 +0900

    Update app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 
 
 
-df = pd.DataFrame(np.random.randn(500,2)/[50,50] + [37.76, -122.4], columns = ['lat','lon']
+df = pd.DataFrame(np.random.randn(500,2)/[50,50] + [37.76, -122.4], columns = ['lat','lon'])
 
 st.map(df)
                  
